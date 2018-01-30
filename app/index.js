50e5a2fb1d348d7f9d353233a867901231e264e1
Andrew Salcedo <asalcedo07@gmail.com>
2018-01-30 13:10:04 +0100

    Add: <Logo> to <Home> Screen

--- app/index.js
+++ app/index.js
@@ -4,7 +4,8 @@ import EStyleSheet from "react-native-extended-stylesheet";
 import Home from "./screens/Home";
 
 EStyleSheet.build({
-	$primaryGreen: "#024b30"
+	$primaryGreen: "#024b30",
+	$white: "#ffffff"
 });
 
 export default () => <Home />;
