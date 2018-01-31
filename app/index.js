a994b0fd3e0c7e1923ac3023445282e564946c09
Andrew Salcedo <asalcedo07@gmail.com>
2018-01-31 20:54:48 +0100

    Add Themes screen. Add iconBackground to <Icon>

--- app/index.js
+++ app/index.js
@@ -2,21 +2,25 @@ import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 
 // import Home from "./screens/Home";
-// import CurrencyList from './screens/CurrencyList'; 
-import Options from './screens/Options'; 
+// import CurrencyList from './screens/CurrencyList';
+// import Options from './screens/Options';
+import Themes from "./screens/Themes";
 
 EStyleSheet.build({
 	$primaryGreen: "#024b30",
+	$primaryBlue: "#323299",
+	$primaryRed: "#663333",
+	$primaryPink: "#FF69B4",
+
 	$white: "#ffffff",
-	$separator: '#E2E2E2',
-	$inputText: '#545454',
-	$lightGrey: '#f0f0f0',
-	$darkText: '#343434',
+	$separator: "#E2E2E2",
+	$inputText: "#545454",
+	$lightGrey: "#f0f0f0",
+	$darkText: "#343434"
 	// $outline: 1
 });
 
 // export default () => <Home />;
 // export default () => <CurrencyList />;
-export default () => <Options />;
-
-
+// export default () => <Options />;
+export default () => <Themes />;
---
+++ app/screens/Themes.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { StatusBar, ScrollView } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
+
+import { ListItem, Separator } from "../components/List";
+
+const styles = EStyleSheet.create({
+	$green: "$primaryGreen",
+	$blue: "$primaryBlue",
+	$red: "$primaryRed",
+	$pink: "$primaryPink"
+});
+
+class Themes extends Component {
+	handleThemePress = color => {
+		console.log("theme changed", color);
+	};
+
+	render() {
+		return (
+			<ScrollView>
+				<StatusBar translucent={false} barStyle="default" />
+				<ListItem
+					text="Green"
+					onPress={() => this.handleThemePress(styles.$green)}
+					selected
+					checkmark={false}
+					iconBackground={styles.$green}
+				/>
+				<Separator />
+				<ListItem
+					text="Blue"
+					onPress={() => this.handleThemePress(styles.$blue)}
+					selected
+					checkmark={false}
+					iconBackground={styles.$blue}
+				/>
+				<Separator />
+                <ListItem 
+                    text="Red" 
+                    onPress={() => this.handleThemePress(styles.$red)} 
+                    selected 
+                    checkmark={false} 
+                    iconBackground={styles.$red} 
+                />
+				<Separator />
+				<ListItem
+					text="Pink"
+					onPress={() => this.handleThemePress(styles.$pink)}
+					selected
+					checkmark={false}
+					iconBackground={styles.$pink}
+				/>
+				<Separator />
+			</ScrollView>
+		);
+	}
+}
+
+export default Themes;
