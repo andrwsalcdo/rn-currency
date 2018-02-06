98e9d8f3e619496d4580e9c418e19d3b3905398a
Andrew Salcedo <asalcedo07@gmail.com>
2018-02-06 16:04:14 +0100

    Change currency data to include full name in currencyList screen

--- app/data/currencies.js
+++ app/data/currencies.js
@@ -1,34 +1,170 @@
 export default [
-	"AUD",
-	"BGN",
-	"BRL",
-	"CAD",
-	"CHF",
-	"CNY",
-	"CZK",
-	"DKK",
-	"EUR",
-	"GBP",
-	"HKD",
-	"HRK",
-	"HUF",
-	"IDR",
-	"ILS",
-	"INR",
-	"JPY",
-	"KRW",
-	"MXN",
-	"MYR",
-	"NOK",
-	"NZD",
-	"PHP",
-	"PLN",
-	"RON",
-	"RUB",
-	"SEK",
-	"SGD",
-	"THB",
-	"TRY",
-	"USD",
-	"ZAR"
+	{
+		symbol: "AUD",
+		name: "Australian Dollar"
+	},
+	{
+		symbol: "BGN",
+		name: "Bulgarian lev"
+	},
+	{
+		symbol: "BRL",
+		name: "Brazilian real"
+	},
+	{
+		symbol: "CAD",
+		name: "Canadian dollar"
+	},
+	{
+		symbol: "CHF",
+		name: "Swiss franc"
+	},
+	{
+		symbol: "CNY",
+		name: "Chinese yuan renminbi"
+	},
+	{
+		symbol: "CZK",
+		name: "Czech koruna"
+	},
+	{
+		symbol: "DKK",
+		name: "Danish krone"
+	},
+	{
+		symbol: "EUR",
+		name: "Euro"
+	},
+	{
+		symbol: "GBP",
+		name: "Pound sterling"
+	},
+	{
+		symbol: "HKD",
+		name: "Hong Kong dollar"
+	},
+	{
+		symbol: "HRK",
+		name: "Croatian kuna"
+	},
+	{
+		symbol: "HUF",
+		name: "Hungarian forint"
+	},
+	{
+		symbol: "IDR",
+		name: "Indonesian rupiah"
+	},
+	{
+		symbol: "ILS",
+		name: "Israeli shekel"
+	},
+	{
+		symbol: "INR",
+		name: "Indian rupee"
+	},
+	{
+		symbol: "JPY",
+		name: "Japanese yen"
+	},
+	{
+		symbol: "KRW",
+		name: "South Korean won"
+	},
+	{
+		symbol: "MXN",
+		name: "Mexican peso"
+	},
+	{
+		symbol: "MYR",
+		name: "Malaysian ringgit"
+	},
+	{
+		symbol: "NOK",
+		name: "Norwegian krone"
+	},
+	{
+		symbol: "NZD",
+		name: "New Zealand dollar"
+	},
+	{
+		symbol: "PHP",
+		name: "Philippine piso"
+	},
+	{
+		symbol: "PLN",
+		name: "Polish zloty"
+	},
+	{
+		symbol: "RON",
+		name: "Romanian leu"
+	},
+	{
+		symbol: "RUB",
+		name: "Russian rouble"
+	},
+	{
+		symbol: "SEK",
+		name: "Swedish krona"
+	},
+	{
+		symbol: "SGD",
+		name: "Singapore dollar"
+	},
+	{
+		symbol: "THB",
+		name: "Thai baht"
+	},
+	{
+		symbol: "TRY",
+		name: "Turkish lira"
+	},
+	{
+		symbol: "USD",
+		name: "US Dollar"
+	},
+	{
+		symbol: "ZAR",
+		name: "South African rand"
+	}
 ];
+
+// export default [
+// 	"AUD",
+// 	"BGN",
+// 	"BRL",
+// 	"CAD",
+// 	"CHF",
+// 	"CNY",
+// 	"CZK",
+// 	"DKK",
+// 	"EUR",
+// 	"GBP",
+
+// 	"HKD",
+// 	"HRK",
+// 	"HUF",
+// 	"IDR",
+// 	"ILS",
+// 	"INR",
+
+// 	"JPY",
+// 	"KRW",
+// 	"MXN",
+
+// 	"MYR",
+// 	"NOK",
+// 	"NZD",
+// 	"PHP",
+
+// 	"PLN",
+// 	"RON",
+// 	"RUB",
+// 	"SEK",
+// 	"SGD",
+
+// 	"THB",
+// 	"TRY",
+// 	"USD",
+// 	"ZAR"
+// ];
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,51 +1,51 @@
 import React, { Component } from "react";
 import { View, Text, FlatList, StatusBar } from "react-native";
-import { connect } from 'react-redux'; 
-import { ListItem, Separator } from '../components/List'; 
-import currencies from '../data/currencies'; 
+import { connect } from "react-redux";
+import { ListItem, Separator } from "../components/List";
+import currencies from "../data/currencies";
 
-import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies'; 
-
+import { changeBaseCurrency, changeQuoteCurrency } from "../actions/currencies";
 
 class CurrencyList extends Component {
-    handlePress = (currency) => {
-        const { type } = this.props.navigation.state.params; 
-        if (type === 'base') {
-            this.props.dispatch(changeBaseCurrency(currency))
-        } else if (type === 'quote') {
-            this.props.dispatch(changeQuoteCurrency(currency))
-        }
-        this.props.navigation.goBack(null); 
-    }
+	handlePress = currency => {
+		const { type } = this.props.navigation.state.params;
+		if (type === "base") {
+			this.props.dispatch(changeBaseCurrency(currency));
+		} else if (type === "quote") {
+			this.props.dispatch(changeQuoteCurrency(currency));
+		}
+		this.props.navigation.goBack(null);
+	};
 
-    render() {
-        const { currentCurrency, primaryColor } = this.props; 
-        return (    
-            <View style={{flex: 1}}>
-                <StatusBar barStyle="default" translucent={false} />
-                <FlatList
-                    data={currencies}
-                    renderItem={({ item }) => (
-                        <ListItem 
-                            text={item}
-                            selected={item === currentCurrency}
-                            onPress={() => this.handlePress(item)}  
-                            iconBackground={primaryColor}  
-                        />
-                    )}
-                    keyExtractor={item => item}
-                    ItemSeparatorComponent={Separator}
-                />
-            </View>
-        )
-    }
+	render() {
+		const { currentCurrency, primaryColor } = this.props;
+		return (
+			<View style={{ flex: 1 }}>
+				<StatusBar barStyle="default" translucent={false} />
+				<FlatList
+					data={currencies}
+					renderItem={({ item }) => (
+						<ListItem
+							currencySymbol={item.symbol}
+							currencyName={item.name}
+							selected={item.symbol === currentCurrency}
+							onPress={() => this.handlePress(item.symbol)}
+							iconBackground={primaryColor}
+						/>
+					)}
+					keyExtractor={item => item.symbol}
+					ItemSeparatorComponent={Separator}
+				/>
+			</View>
+		);
+	}
 }
 
 const mapStateToProps = (state, ownProps) => ({
-    baseCurrency: state.currencies.baseCurrency,
-    quoteCurrency: state.currencies.quoteCurrency,
-    currentCurrency: ownProps.navigation.state.params.type === 'base' ? state.currencies.baseCurrency : state.currencies.quoteCurrency,
-    primaryColor: state.theme.primaryColor, 
-})
+	baseCurrency: state.currencies.baseCurrency,
+	quoteCurrency: state.currencies.quoteCurrency,
+	currentCurrency: ownProps.navigation.state.params.type === "base" ? state.currencies.baseCurrency : state.currencies.quoteCurrency,
+	primaryColor: state.theme.primaryColor
+});
 
 export default connect(mapStateToProps)(CurrencyList);
