4e609ac127a97d564da6539c4fb4d7f1c90d0c12
Andrew Salcedo <asalcedo07@gmail.com>
2018-02-09 14:59:33 +0100

    Add: persist theme data only with redux-persist

--- app/config/store.js
+++ app/config/store.js
@@ -1,20 +1,30 @@
-import { createStore, applyMiddleware } from 'redux'; 
-import logger from 'redux-logger'; 
-import createSagaMiddleware from 'redux-saga'; 
-import reducers from '../reducers'; 
-import rootSaga from './sagas'; 
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import reducers from "../reducers";
+import rootSaga from "./sagas";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
-const sagaMiddleware = createSagaMiddleware(); 
-const middleware = [sagaMiddleware]; 
-
-if (process.env.NODE_ENV === 'development') {
-    middleware.push(logger); 
+if (process.env.NODE_ENV === "development") {
+	middleware.push(logger);
 }
 
+const config = {
+	key: "root",
+    storage, 
+    //persist only theme data
+    whitelist: ['theme']
+};
 
-const store = createStore(reducers, applyMiddleware(...middleware)); 
+const reducer = persistReducer(config, reducers);
 
-sagaMiddleware.run(rootSaga); 
-
-export default store; 
+export default function configureStore() {
+    let store = createStore(reducer, applyMiddleware(...middleware));
+	sagaMiddleware.run(rootSaga);
+	let persistor = persistStore(store);
+	return { persistor, store };
+}
--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 import { Provider } from "react-redux";
 import Navigator from "./config/routes";
-import store from "./config/store";
+import configureStore from "./config/store";
+import { PersistGate } from "redux-persist/es/integration/react";
+
+let { store, persistor } = configureStore();
+
 
 EStyleSheet.build({
 	$primaryGreen: "#024b30",
@@ -14,11 +18,13 @@ EStyleSheet.build({
 	$inputText: "#545454",
 	$lightGrey: "#f0f0f0",
 	$darkText: "#343434"
-	// $outline: 1
 });
+
 
 export default () => (
 	<Provider store={store}>
-		<Navigator />
+		<PersistGate loading="loading..." persistor={persistor}>
+			<Navigator />
+		</PersistGate>
 	</Provider>
-);
+); 
